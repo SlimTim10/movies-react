8fe9491b55cc194b045dd9eb5c4fa0f6a29fc21f
SlimTim10 <slimtim10@gmail.com>
2019-06-25 13:15:49 -0400

    Port static example to react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  getMovie = event => {
+    if (event.key === 'Enter') {
+      const apiKey = '';
+      const rootEndpoint = `http://www.omdbapi.com/?apikey=${apiKey}`;
+      
+      const searchWord = event.target.value;
+
+      const endpoint = rootEndpoint
+            + `&s=${searchWord}`;
+      console.log('Endpoint:', endpoint);
+
+      fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+          const firstResult = data.Search[0];
+
+          const imdbLink = `https://www.imdb.com/title/${firstResult.imdbID}/`;
+
+          console.log(`Title: ${firstResult.Title}`);
+          console.log(`Year: ${firstResult.Year}`);
+          console.log(`Poster: ${firstResult.Poster}`);
+          console.log(`IMDb Link: ${imdbLink}`);
+        });
+    }
+
+  }
+  
+  render() {
+    return (
+      <div>
+        <aside id="ui-control">
+          <h2>Search for a movie</h2>
+          <input onKeyPress={this.getMovie} name="movie" type="text" placeholder="Type a movie name and press Enter" />
+        </aside>
+
+        <main>
+          <p>The movie information will appear below...</p>
+          <article>
+            <section id="movie-title"></section>
+            <section id="movie-year"></section>
+            <section id="movie-link"></section>
+            <section id="movie-image"></section>
+          </article>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
