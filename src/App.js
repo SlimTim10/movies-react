c0b991681006f866344d45b41d685af52063ad1a
SlimTim10 <slimtim10@gmail.com>
2019-07-01 15:23:11 -0400

    Render movie data on page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  getMovie = event => {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      title: '',
+      year: '',
+      image: '',
+      imdbLink: ''
+    };
+  }
+  
+  getMovie = (event) => {
     if (event.key === 'Enter') {
       const apiKey = '';
       const rootEndpoint = `http://www.omdbapi.com/?apikey=${apiKey}`;
@@ -16,20 +27,33 @@ class App extends React.Component {
       fetch(endpoint)
         .then(response => response.json())
         .then(data => {
+          if (data.Response === 'False') {
+            console.log(`Error: ${data.Error}`);
+            return;
+          }
+          
           const firstResult = data.Search[0];
-
           const imdbLink = `https://www.imdb.com/title/${firstResult.imdbID}/`;
 
           console.log(`Title: ${firstResult.Title}`);
           console.log(`Year: ${firstResult.Year}`);
           console.log(`Poster: ${firstResult.Poster}`);
           console.log(`IMDb Link: ${imdbLink}`);
+          
+          this.setState({
+            title: firstResult.Title,
+            year: firstResult.Year,
+            image: firstResult.Poster,
+            imdbLink: imdbLink
+          });
         });
     }
 
   }
   
   render() {
+    const poster = this.state.image && <img src={this.state.image} alt="Poster" />;
+    
     return (
       <div>
         <aside id="ui-control">
@@ -40,10 +64,10 @@ class App extends React.Component {
         <main>
           <p>The movie information will appear below...</p>
           <article>
-            <section id="movie-title"></section>
-            <section id="movie-year"></section>
-            <section id="movie-link"></section>
-            <section id="movie-image"></section>
+            <section id="movie-title">{this.state.title}</section>
+            <section id="movie-year">{this.state.year}</section>
+            <section id="movie-link">{this.state.imdbLink}</section>
+            <section id="movie-image">{poster}</section>
           </article>
         </main>
       </div>
